3eef9cd3b4568a20c3a4a73fcd7ad351286b01f3
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.com>
2020-03-07 07:55:14 +0530

    Updates

---
+++ precache-manifest.4cc731542363deceb1c798178b3304bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d09ee33395bcd4b42684e481eb37eaa4",
+    "url": "/Robo-friends/index.html"
+  },
+  {
+    "revision": "257a9f694397c102db18",
+    "url": "/Robo-friends/static/css/main.73b3aea7.chunk.css"
+  },
+  {
+    "revision": "ea1e8636fc00aac046f1",
+    "url": "/Robo-friends/static/js/2.a9db7664.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Robo-friends/static/js/2.a9db7664.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "257a9f694397c102db18",
+    "url": "/Robo-friends/static/js/main.62b58eec.chunk.js"
+  },
+  {
+    "revision": "286cbfe6c1182ba2b240",
+    "url": "/Robo-friends/static/js/runtime-main.37d183b0.js"
+  }
+]);
